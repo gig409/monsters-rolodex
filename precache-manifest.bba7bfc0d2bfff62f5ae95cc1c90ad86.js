e2da51a4288a6d7dcddf499b171e5ee2dceef659
gig409 <garfield.gordon@brainchildeducation.com>
2020-04-09 11:16:15 +0100

    Updates

---
+++ precache-manifest.bba7bfc0d2bfff62f5ae95cc1c90ad86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f690c36ac1b7fb2e4990df22512f1916",
+    "url": "/gig409/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1f969c0aa519cb0b0ab2",
+    "url": "/gig409/monsters-rolodex/static/css/main.2c19e447.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/gig409/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gig409/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f969c0aa519cb0b0ab2",
+    "url": "/gig409/monsters-rolodex/static/js/main.64ab14da.chunk.js"
+  },
+  {
+    "revision": "0292d7d0bd9ed4d2474d",
+    "url": "/gig409/monsters-rolodex/static/js/runtime-main.071c2574.js"
+  }
+]);
